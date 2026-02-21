a33d6d98fba73f868d7dc8f2026ec8bce0db277e
ALP300 <aitorpalomino1@gmail.com>
2026-02-20 21:38:56 -0500

    as

---
+++ src/controllers/productosController.js
@@ -0,0 +1,11 @@
+import pool from "../config/database.js";
+
+export async function getProductos(req, res) {
+    try {
+        const result = await pool.query('SELECT * FROM productos');
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener productos', error);
+        res.status(500).json({ error: 'Error al obtener productos' });
+    }
+}
