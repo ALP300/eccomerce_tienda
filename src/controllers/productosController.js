63553de11b911a0a99fb154ea0642e63a9850965
ALP300 <aitorpalomino1@gmail.com>
2026-02-20 21:52:21 -0500

    sad

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-import pool from "../config/database";      
-
-export const getProductos = async (req, res) => {
-=======
 import pool from "../config/database.js";
 
 export async function getProductos(req, res) {
->>>>>>> 02e8c3be900de3df971945dedbf37618dbb2c327
+
     try {
         const result = await pool.query('SELECT * FROM productos');
         res.json(result.rows);
     } catch (error) {
-<<<<<<< HEAD
+
         console.error('Error al obtener los productos', error);
         res.status(500).json({ error: 'Error al obtener los productos' });
     }
 };
         
-=======
-        console.error('Error al obtener productos', error);
-        res.status(500).json({ error: 'Error al obtener productos' });
-    }
-}
->>>>>>> 02e8c3be900de3df971945dedbf37618dbb2c327
--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
+app.use('/api', ecommerceRoutes);
+
 conectar()
